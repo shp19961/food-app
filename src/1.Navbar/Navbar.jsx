bebb541b94d87d0b826a9dfba02bcbe165705ae0
shubham-allprojects <parab16aug@gmail.com>
2022-06-25 19:01:33 +0530

    ok

--- src/1.Navbar/Navbar.jsx
+++ src/1.Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-scroll";
 
 const Navbar = () => {
   useEffect(() => {
-    let nav = document.querySelector(".navigation-wrap ");
+    let nav = document.querySelector(".navigation-wrap");
     window.onscroll = function () {
       if (document.documentElement.scrollTop > 20) {
         nav.classList.add("scroll-on");
